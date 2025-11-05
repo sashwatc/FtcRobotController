57d53df24ff32172dad93918c13f420ae465448a
sashwatc <sashwatc.7@gmail.com>
2025-11-05 16:37:34 -0600

    Fieldrelative

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumFieldOrientatedOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.mechanisms.MechanumDrive;
+public class MechanumFieldOrientatedOpMode extends OpMode {
+
+    MechanumDrive drive = new MechanumDrive();
+    double forward, strafe, rotate;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFeildRelative(forward, strafe, rotate);
+    }
+}
