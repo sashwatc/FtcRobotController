8db9130bb5b2b883da6b8a1fa5a6b90ef7d6805c
sashwatc <sashwatc.7@gmail.com>
2025-11-06 21:54:33 -0600

    KitbotOpv1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KitBotOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KitBotOpMode.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.mechanisms.MechanumDrive;
+@TeleOp(name ="FieldOrientedOp")
 public class MechanumFieldOrientatedOpMode extends OpMode {
 
     MechanumDrive drive = new MechanumDrive();
