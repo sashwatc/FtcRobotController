29e2f2bdd00574a30e638107ef17b771e287e255
sashwatc <sashwatc.7@gmail.com>
2025-11-08 00:08:09 -0600

    used LLResults

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
@@ -1,25 +1,42 @@
 package org.firstinspires.ftc.teamcode.tests;
 
-
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.vision.Limelight;
-
-@TeleOp(name = "Limelight Test")
+@TeleOp(name="Limelight Test")
 public class LimelightTest extends LinearOpMode {
+
+    private Limelight3A limelight;
+
     @Override
     public void runOpMode() {
-        Limelight limelight = new Limelight("172.29.0.1");
+        // Initialize Limelight
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // poll 100 times/sec
+        limelight.start();            // start the Limelight
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("Has Target", limelight.hasTarget());
-            telemetry.addData("TY", limelight.getTY());
-            telemetry.addData("Target ID", limelight.getTargetID());
+            LLResult result = limelight.getLatestResult();
+
+            if (result != null && result.isValid()) {
+                // Target is visible
+                telemetry.addData("Target Visible", true);
+                telemetry.addData("X Offset (tx)", result.getTx());
+                telemetry.addData("Y Offset (ty)", result.getTy());
+                telemetry.addData("Area (ta)", result.getTa());
+            } else {
+                // No target
+                telemetry.addData("Target Visible", false);
+            }
+
             telemetry.update();
-            sleep(100);
         }
     }
 }
