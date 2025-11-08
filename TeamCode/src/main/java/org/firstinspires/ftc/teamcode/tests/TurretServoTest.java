eff891a61bf5b78228979dbcb7bdca3485ff14cd
sashwatc <sashwatc.7@gmail.com>
2025-11-07 20:59:11 -0600

    added TurretServoTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TurretServoTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.KitTurretControl;
+
+public class TurretServoTest extends OpMode {
+    KitTurretControl turret = new KitTurretControl();
+
+    @Override
+    public void init() {
+        turret.init(hardwareMap);
+        telemetry.addLine("✅ Turret Servo Test Ready");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        double stick = -gamepad2.right_stick_y;
+        turret.turretPitch(stick);
+        telemetry.addData("Stick", stick);
+        telemetry.update();
+    }
+}
+
+
