4c966a15fb82b1852b1039cf4f11db1dbf6f5d97
sashwatc <sashwatc.7@gmail.com>
2025-11-14 17:43:32 -0600

    Brogan Pratt Toturial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
@@ -1,63 +1,55 @@
 package org.firstinspires.ftc.teamcode.tests;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.SerialNumber;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-@TeleOp(name="Limelight Test")
-public class LimelightTest extends LinearOpMode {
+@Autonomous
+public class LimelightTest extends OpMode{
 
     private Limelight3A limelight;
+    private IMU imu;
 
     @Override
-    public void runOpMode() {
-        // Initialize Limelight
+    public void init() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.setPollRateHz(100);
+        limelight.pipelineSwitch(0); //change
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+    }
+
+    @Override
+    public void start() {
         limelight.start();
+    }
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+    @Override
+    public void loop(){
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llResult = limelight.getLatestResult();
+        if (llResult != null && llResult.isValid()){
+            Pose3D botPose = llResult.getBotpose_MT2();
+            telemetry.addData("Tx", llResult.getTx());
+            telemetry.addData("Ty", llResult.getTy());
+            telemetry.addData("Target Area", llResult.getTa());
+            telemetry.addData("BotPose", botPose.toString());
+            telemetry.addData("Yaw", botPose.getOrientation().getYaw());
 
-        waitForStart();
-
-        while (opModeIsActive()) {
-            LLResult result = limelight.getLatestResult();
-
-            if (result != null && result.isValid()) {
-                telemetry.addData("Target Visible", true);
-                telemetry.addData("X Offset (tx)", result.getTx());
-                telemetry.addData("Y Offset (ty)", result.getTy());
-                telemetry.addData("Area (ta)", result.getTa());
-
-                // Parse the JSON string to get AprilTag ID
-                // Parse the JSON string to get AprilTag ID
-                try {
-                    JSONObject json = new JSONObject(result.toString());
-                    JSONArray fiducials = json.optJSONArray("Fiducial"); // array of tags
-                    if (fiducials != null && fiducials.length() > 0) {
-                        JSONObject firstTag = fiducials.getJSONObject(0);
-                        // Try both keys in case SDK uses "fid" or "id"
-                        int tagId = firstTag.optInt("fid", firstTag.optInt("id", -1));
-                        telemetry.addData("AprilTag ID", tagId);
-                    } else {
-                        telemetry.addData("AprilTag ID", "None detected");
-                    }
-                } catch (JSONException e) {
-                    telemetry.addData("AprilTag ID", "Error parsing JSON");
-                }
-
-            } else {
-                telemetry.addData("Target Visible", false);
-                telemetry.addData("AprilTag ID", "N/A");
-            }
-
-            telemetry.update();
         }
     }
+
 }
